1d87c44edb410766c176e49d55628d832a1179f1
MaxDanna26 <maximilianodanna@hotmail.com>
2025-05-22 22:12:01 +0200

    changed package

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
+  },
   build: {
     rollupOptions: {
-      external: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-    },
-  },
+      external: []
+    }
+  }
 });
